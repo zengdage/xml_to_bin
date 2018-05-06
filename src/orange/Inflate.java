0a2e6b13b970c92112d6390baeb770b32d5afe55
zengzhijin <zhijinzeng@allwinnertech.com>
2018-05-06 17:33:36 +0800

    delete some useless files

--- src/orange/Inflate.java
+++ src/orange/Inflate.java
@@ -12,37 +12,12 @@ import xmlpulldemo.LayoutInflate;
 
 public class Inflate {
 
-//	public static void main(String[] args) {
-//		Basewin basewin = getLayout("layout.xml");
-//		basewin.printChild(0);
-//		System.out.println("end!");
-//		System.out.println("***************Melis Code***************");
-//		//outMelisCode(basewin);
-//		System.out.println("****************************************");
-//	}
+
 	
 	public static Basewin getLayout(DataFileXmlItem xmlItem , String str) {
 		LayoutInflate inflate=new LayoutInflate(xmlItem);
 		return inflate.inflate(str, null);
 	}
 	
-	
-	
-	public static void outMelisCode(Basewin win,String codePath) {	
-		  try {    
-			  	File file = new File(codePath);
-				if (!file.exists()) {
-					file.createNewFile();
-				}
-				System.out.println(file.getAbsolutePath());
-	            PrintStream ps = new PrintStream(new FileOutputStream(file));  
-	            win.outCode(win, null,null,ps);
-	            ps.close();
-	        } catch (Exception e) {  
-	            e.printStackTrace();  
-	        }  
-		
-	}
-	
-	
+
 }
--- src/view/SListBox.java
+++ src/view/SListBox.java
@@ -137,72 +137,7 @@ public class SListBox extends Basewin{
 	
 	public void outCtrCode(Basewin win, Basewin parent,PrintStream ps) {
 		StringBuffer code = new StringBuffer();
-	
-		code.append("static H_WIN "+win.win_name+"ctl_create(H_WIN parent, button_para_t *para)"+"\n");
-		code.append("{"+"\n");
-		code.append("\tH_WIN h_ctrl;"+"\n");
-		code.append("\tRECT rect;"+"\n");
-		code.append("\t__gui_ctlwincreate_para_t create_para;"+"\n");
-		code.append("\teLIBs_memset(&create_para, 0, sizeof(__gui_ctlwincreate_para_t));"+"\n");
-		code.append("\tGUI_WinGetClientRECT(parent, &rect);"+"\n");
-		
-		
-		code.append("\tcreate_para.dwStyle ="+win.win_dwStyle+";"+"\n");
-		code.append("\tcreate_para.dwExStyle ="+win.win_dwExStyle+";"+"\n");
-		
-		code.append("\tcreate_para.ctl_rect.x ="+win.win_left+";"+"\n");
-		code.append("\tcreate_para.ctl_rect.y"+win.win_top+";"+"\n");
-		code.append("\tcreate_para.ctl_rect.width ="+(win.win_right-win.win_left)+";"+"\n");
-		code.append("\tcreate_para.ctl_rect.height ="+(win.win_bottom-win.win_top)+";"+"\n");
-		code.append("\tcreate_para.spClassName = "+win.spClassName+";"+"\n");
-		code.append("\tcreate_para.hParent = parent;"+"\n");
-		code.append("\tcreate_para.id = "+win_id+";"+"\n");
-		code.append("\tcreate_para.attr = para;"+"\n");
 
-		
-		code.append("\th_ctrl = GUI_CtrlWinCreate(&create_para);"+"\n");
-		code.append("\tif( !h_ctrl )"+"\n");
-		code.append("\t{"+"\n");
-		code.append("\t\t__err(\" static date control win create error \\n\");"+"\n");
-		code.append("\t}"+"\n");
-		code.append("\treturn h_ctrl;"+"\n");
-		code.append("}"+"\n");
-		code.append(""+"\n");
-		code.append(""+"\n");
-		ps.print(code);
-		
-		
-		System.out.println("static H_WIN "+win.win_name+"ctl_create(H_WIN parent, button_para_t *para)");
-		System.out.println("{");
-		System.out.println("\tH_WIN h_ctrl;");
-		System.out.println("\tRECT rect;");
-		System.out.println("\t__gui_ctlwincreate_para_t create_para;");
-		System.out.println("\teLIBs_memset(&create_para, 0, sizeof(__gui_ctlwincreate_para_t));");
-		System.out.println("\tGUI_WinGetClientRECT(parent, &rect);");
-		
-		
-		System.out.println("\tcreate_para.dwStyle ="+win.win_dwStyle+";");
-		System.out.println("\tcreate_para.dwExStyle ="+win.win_dwExStyle+";");
-		
-		System.out.println("\tcreate_para.ctl_rect.x ="+win.win_left+";");
-		System.out.println("\tcreate_para.ctl_rect.y"+win.win_top+";");
-		System.out.println("\tcreate_para.ctl_rect.width ="+(win.win_right-win.win_left)+";");
-		System.out.println("\tcreate_para.ctl_rect.height ="+(win.win_bottom-win.win_top)+";");
-		System.out.println("\tcreate_para.spClassName = "+win.spClassName+";");
-		System.out.println("\tcreate_para.hParent = parent;");
-		System.out.println("\tcreate_para.id = "+win_id+";");
-		System.out.println("\tcreate_para.attr = para;");
-
-		
-		System.out.println("\th_ctrl = GUI_CtrlWinCreate(&create_para);");
-		System.out.println("\tif( !h_ctrl )");
-		System.out.println("\t{");
-		System.out.println("\t\t__err(\" static date control win create error \\n\");");
-		System.out.println("\t}");
-		System.out.println("\treturn h_ctrl;");
-		System.out.println("}");
-		System.out.println("");
-		System.out.println("");
 		
 	}
 
